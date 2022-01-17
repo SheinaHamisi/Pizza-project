95c44ea2749d10c9d66d08ac29d795dfa8f690f9
SheinaHamisi <hamisisheina@gmail.com>
2022-01-17 16:12:33 +0300

    added default pizza classes

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+class Pizza{
+    constructor(name){
+        this.name = name;
+    }
+}
+
+class Order extends Pizza{
+    constructor(name, size, quantity,crust,orderType ){
+        super(name);
+        this.size = size;
+        this.quantity = quantity;
+        this.crust = crust;
+        this.orderType = orderType;
+    }
+}
+
 $('#barbeque').click(function(){
     
 })
